510b422973ea127953de97bc2ab2cbdd26e4bd80
wnaina <39332359+nainaw@users.noreply.github.com>
2026-07-14 14:41:06 +0300

    Add header information to scribe.rb

--- tools/scribe.rb
+++ tools/scribe.rb
@@ -1,1 +1,7 @@
+#!/usr/bin/env ruby
 
+puts "===================================="
+puts " Scribe"
+puts " Tsianjery Publishing System"
+puts " Version 1.0"
+puts "===================================="
