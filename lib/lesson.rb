4355508954e718637aae9d3ca66e068ea5759071
wnaina <39332359+nainaw@users.noreply.github.com>
2026-07-15 16:08:33 +0300

    Create Lesson class with attributes and initializer

---
+++ lib/lesson.rb
@@ -0,0 +1,15 @@
+class Lesson
+
+  attr_accessor :title
+  attr_accessor :verse
+  attr_accessor :reference
+
+  def initialize(title, verse, reference)
+
+    @title = title
+    @verse = verse
+    @reference = reference
+
+  end
+
+end
