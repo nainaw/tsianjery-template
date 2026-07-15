b0a9839aa75ea608a28235c0a38c73fa44e58f02
wnaina <39332359+nainaw@users.noreply.github.com>
2026-07-15 16:17:42 +0300

    Add Quarter class with title, subtitle, and lessons

--- lib/quarter.rb
+++ lib/quarter.rb
@@ -1,1 +1,21 @@
+class Quarter
 
+  attr_accessor :title
+  attr_accessor :subtitle
+  attr_accessor :lessons
+
+  def initialize(title, subtitle)
+
+    @title = title
+    @subtitle = subtitle
+    @lessons = []
+
+  end
+
+  def add_lesson(lesson)
+
+    @lessons << lesson
+
+  end
+
+end
